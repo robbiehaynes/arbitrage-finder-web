7a213d21a0e56375d54bf5cd4aecd7f9ca75e06f
Robert Haynes <robhaynes0420@gmail.com>
2024-04-03 19:32:30 +0100

    Consolidated code

--- api/index.py
+++ api/index.py
@@ -1,14 +1,99 @@
 import os
+import certifi
+import bson
+import json
 from dotenv import load_dotenv, find_dotenv
 
 from flask import Flask, jsonify, request
 
 from authlib.integrations.flask_oauth2 import ResourceProtector
-from validator import Auth0JWTBearerTokenValidator
+from urllib.request import urlopen
 
-from models import Arbitrage
+from authlib.oauth2.rfc7523 import JWTBearerTokenValidator
+from authlib.jose.rfc7517.jwk import JsonWebKey
+
+from pymongo import MongoClient
 
 load_dotenv(find_dotenv('.env.local'))
+
+class Auth0JWTBearerTokenValidator(JWTBearerTokenValidator):
+  def __init__(self, domain, audience):
+      issuer = f"https://{domain}/"
+      jsonurl = urlopen(f"{issuer}.well-known/jwks.json", cafile=certifi.where())
+      public_key = JsonWebKey.import_key_set(
+          json.loads(jsonurl.read())
+      )
+      super(Auth0JWTBearerTokenValidator, self).__init__(
+          public_key
+      )
+      self.claims_options = {
+          "exp": {"essential": True},
+          "aud": {"essential": True, "value": audience},
+          "iss": {"essential": True, "value": issuer},
+      }
+  
+class Arbitrage:
+  """Arbitrage Model"""
+  def __init__(self):
+    return
+
+  def create(self, bet):
+    """Create a new bet"""
+    book = self.get_by_teams_and_time(bet["home_team"], bet["away_team"], bet["time"])
+    if book:
+      return
+    new_book = db.h2h.insert_one(bet)
+    return self.get_by_id(new_book.inserted_id)
+
+  def get_all(self):
+    """Get all arbs"""
+    arbs = db.h2h.find()
+    return [{**arb, "_id": str(arb["_id"])} for arb in arbs]
+  
+  def get_by_sport(self, sport):
+    """Get all arbs by sport"""
+    arbs = db.h2h.find({"sport": sport})
+    return [{**arb, "_id": str(arb["_id"])} for arb in arbs]
+
+  def get_by_id(self, id):
+      """Get a arbitrage by id"""
+      arb = db.h2h.find_one({"_id": bson.ObjectId(id)})
+      if not arb:
+          return
+      arb["_id"] = str(arb["_id"])
+      return arb
+
+  def get_by_teams_and_time(self, home_team, away_team, time):
+      """Get a arbitrage given its title and author"""
+      arb = db.h2h.find_one({"home_team": home_team, "away_team": away_team, "time": time})
+      if not arb:
+          return
+      arb["_id"] = str(arb["_id"])
+      return arb
+
+  def update(self, arb_id, away_team="", away_stake={}, home_team="", home_stake={}, roi=""):
+      """Update a arbitrage"""
+      data={}
+      if away_team: data["away_team"]=away_team
+      if away_stake: data["away_stake"]=away_stake
+      if home_team: data["home_team"]=home_team
+      if home_stake: data["home_stake"]=home_stake
+      if roi: data["roi"]=roi
+
+      book = db.h2h.update_one(
+          {"_id": bson.ObjectId(arb_id)},
+          {
+              "$set": data
+          }
+      )
+      book = self.get_by_id(arb_id)
+      return book
+
+  def delete(self, arb_id):
+      """Delete a book"""
+      arb = db.h2h.delete_one({"_id": bson.ObjectId(arb_id)})
+      return arb
+
 
 require_auth = ResourceProtector()
 validator = Auth0JWTBearerTokenValidator(
@@ -20,6 +105,8 @@ require_auth.register_token_validator(validator)
 app = Flask(__name__)
 
 # MongoDB connection information
+client = MongoClient(os.getenv('MONGO_DB_URI'), tls=True, tlsCAFile=certifi.where())
+db = client.arbitrages
 DB_NAME = "arbitrages"
 COLLECTION_NAME = "h2h"
 
